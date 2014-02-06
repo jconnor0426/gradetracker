b76c505df955ef6df9308bbc8746fa7546386b13
connor <connor@PyServer.(none)>
2014-02-06 17:15:33 -0500

    Got a basic view for students showing what courses they are enrolled in

--- GradeTracker/models.py
+++ GradeTracker/models.py
@@ -5,8 +5,12 @@ class Student(models.Model):
 	fName = models.CharField( max_length=200 )
 	lName = models.CharField( max_length=200 )
 	Institution = models.CharField( max_length=200 )
+	def __str__(self):
+		return self.fName + ' ' + self.lName
 
 class Course(models.Model):
-	student = models.ForeignKey(Student)
+	student = models.ForeignKey('Student')
 	course_name  = models.CharField(max_length=200)
 	course_code  = models.CharField(max_length=200)
+	def __str__(self):
+		return   self.student.fName + '-' + self.course_name
--- GradeTracker/views.py
+++ GradeTracker/views.py
@@ -1,1 +1,13 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from GradeTracker.models import Student, Course
+
+def detail(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    return render(request, 'GradeTracker/detail.html', {'student': student})
+def index(request):
+    student_list = Student.objects.all()
+    context = {'student_list': student_list }
+    return render( request, 'GradeTracker/index.html', context )  
+
