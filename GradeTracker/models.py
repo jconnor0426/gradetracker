7f78fb6dd8f4c29e0f53709f893f4d60b0faa6e8
GucciMain() <j.connor.mclaughlin@gmail.com>
2014-02-11 13:42:41 -0500

    Added three models: Graded_Activities, SubGraded_Activities, and Templates; also added views for them in admin.py

--- GradeTracker/models.py
+++ GradeTracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Student(models.Model):
@@ -14,3 +15,29 @@ class Course(models.Model):
 	course_code  = models.CharField(max_length=200)
 	def __str__(self):
 		return   self.student.fName + '-' + self.course_name
+
+class Graded_Activities(models.Model):
+	student = models.ForeignKey('student')
+	activity_name = models.CharField(max_length=200)
+	#set grade_weight minimum and maximum from 0-100% represented as 0-1
+	grade_weight = models.FloatField(validators = [MinValueValidator(0.00), MaxValueValidator(1.00)])
+	def __str__(self):
+		return self.student.fName + '-' + self.activity_name + ':' + str(self.grade_weight)
+
+class SubGraded_Activities(models.Model):
+	main_category = models.ForeignKey('Graded_Activities')
+	subactivity_name = models.CharField(max_length=200)
+	subgrade_weight = models.FloatField(validators = [MinValueValidator(0.00), MaxValueValidator(1.00)])
+	def __str__(self):
+		return (self.main_category.activity_name + ':' +  str(self.main_category.grade_weight) + 
+		'     ' + self.subactivity_name + ':' + str(self.subgrade_weight))
+
+class Templates(models.Model):
+        course = models.ForeignKey('Course')
+	graded_activities = models.ForeignKey('Graded_Activities')
+	#need to import all activity names and activity weights assocatied with a particular course
+	
+        institution = models.CharField(max_length=200)
+        def __str__(self):
+                return (self.course.course_name  + " : " + self.Institution)
+		
--- GradeTracker/views.py
+++ GradeTracker/views.py
@@ -1,11 +1,21 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from GradeTracker.models import Student, Course
+from GradeTracker.forms import courseAdd
 
 def detail(request, student_id):
     student = get_object_or_404(Student, pk=student_id)
-    return render(request, 'GradeTracker/detail.html', {'student': student})
+    if request.method == 'POST': # If the form has been submitted...
+        form = courseAdd(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            student.course_set.create( course_name = form.cleaned_data['courseName'], course_code = form.cleaned_data['courseCode'] )            
+            return HttpResponseRedirect('/GT/' + str(student_id) ) # Redirect after POST
+    else:
+        form = courseAdd() # An unbound form
+    return render(request, 'GradeTracker/detail.html', {'student': student , 'form': form })
+
 def index(request):
     student_list = Student.objects.all()
     context = {'student_list': student_list }
