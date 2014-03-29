d956bd81b9ed887636a31c30df3f5aa963f082f7
Connor McLaughlin <Connor@wg-dhcp201d200d106.secure.wireless.fsu.edu>
2014-03-29 17:02:34 -0400

    hopefully this ends it all

--- djangogcal/__init__.py
+++ djangogcal/__init__.py
@@ -1,6 +1,25 @@
-class annoying:
-	def __init__ (self ):
-		while 1:
-			print "Im Annoying"
+from django.conf import settings
 
-a = annoying()
+from djangogcal.adapter import CalendarAdapter, CalendarEventData
+from djangogcal.observer import CalendarObserver
+
+from GradeTracker.models import Graded_Activities
+
+class GradedCalendarAdapter(CalendarAdapter):
+    """
+    A calendar adapter for the Showing model.
+    """
+    
+    def get_event_data(self, instance):
+        """
+        Returns a CalendarEventData object filled with data from the adaptee.
+        """
+        return CalendarEventData(
+            start=instance.grade_due_date,
+            end=instance.grade_due_date,
+            title=instance.activity_name
+        )
+
+observer = CalendarObserver(email="alexdmail15@gmail.com",
+                            password="vpmqesd43")
+observer.observe(Graded_Activities, GradedCalendarAdapter())
