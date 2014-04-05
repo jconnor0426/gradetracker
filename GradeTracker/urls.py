26f3d09f2a3ee0b44692e244331991f0a82f54c9
Connor McLaughlin <Connor@wg-dhcp201d200d137.secure.wireless.fsu.edu>
2014-04-05 16:07:38 -0400

    more work on template debugging

--- GradeTracker/urls.py
+++ GradeTracker/urls.py
@@ -34,7 +34,7 @@ urlpatterns = patterns('',
     url(r'^google/$', views.googleTest, name="googleTest"),
 
     #Templating System
-    url(r'^searchtemplate/$', views.searchTemplate, name="searchTemplate"),
+    url(r'^searchtemplate/$', views.searchTemplateView, name="searchTemplate"),
 
     #user auth urls --> login and logout
     url(r'^accounts/login/$', views.login, name='login'),
