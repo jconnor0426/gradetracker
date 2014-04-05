213203c7dcee624cf81709fe2a6628818401a0ab
Connor McLaughlin <Connor@wg-dhcp201d200d137.secure.wireless.fsu.edu>
2014-04-05 16:05:47 -0400

    search template added to the main view imports

--- GradeTracker/courseTemplatesView.py
+++ GradeTracker/courseTemplatesView.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, render_to_response
 from django.contrib import auth
 from django.core.context_processors import csrf
+from django.contrib.auth.decorators import login_required
 #cross-site request forgery --> method for people hacking into website
 from GradeTracker.models import Student, Course, Graded_Activities, SubGraded_Activities, Templates
 from GradeTracker.forms import courseAdd, activityAdd, subactivityAdd, MyRegistrationForm, templateSearch
