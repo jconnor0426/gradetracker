03d250df9a28cd92cf4a68ba181312aa51cdd35b
Connor McLaughlin <Connor@Connors-MacBook-Pro.local>
2014-04-06 15:31:22 -0400

    changed some code so that users will not have null grades

--- GradeTracker/utilities.py
+++ GradeTracker/utilities.py
@@ -13,12 +13,19 @@ def calculateClassGrade( course ):
 	classGrade = 0
 	#Go through all the activities
 	for activity in course.graded_activities_set.all():
+		if ! activity.grade_earned:
+			activity.grade_earned = 0
+			activity.save()
 		classGrade += activity.grade_weight * activity.grade_earned
 	return classGrade
 
 def activityGradeFromSubActivities( activity ):
 	activitygrade = 0
 	for each in activity.subgraded_activities_set.all():
-		activitygrade += each.subgrade_weight * each.subgrade_earned
-		activity.grade_earned = activitygrade
+		if each.subgrade_earned:
+			activitygrade += each.subgrade_weight * each.subgrade_earned
+		else:
+			each.subgrade_earned = 0
+			each.save()
+	activity.grade_earned = activitygrade
 	activity.save()     
