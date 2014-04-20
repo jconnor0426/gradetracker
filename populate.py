9c7e4c6b43300286140a84123ffdba120cb17ad8
Connor McLaughlin <Connor@wg-dhcp201d201d132.secure.wireless.fsu.edu>
2014-04-20 19:41:46 -0400

    Working on the populate script

--- populate.py
+++ populate.py
@@ -1,43 +1,52 @@
 from django.contrib.auth.models import User
 from GradeTracker.models import Student, Course, Graded_Activities, SubGraded_Activities, Templates
+from datetime import date, timedelta
 
+
+sampleClasses = [ 	("Course1", "COP101"),
+			("Course2", "COP102"), 
+			("Course3", "COP103"),
+			("Course4", "COP104") ]
+
+sample_activities = [ 	"Activity1",
+			"Activity2",
+			"Activity3" ]
+
+sample_subactivities = [ 	"SubActivity1",
+				"SubActivity2",
+				"SubActivity3"]
 
 def populate():
 	#Build the list of data to populate
 	first_names = [line.rstrip() for line in open('actor-givenname')]
 	last_names = [line.rstrip() for line in open('actor-surname')][:len(first_names) ]
 
-	for a,b in zip( first_names, last_names ):
-		print a + '\t' + b
-
-"""
-	#For each in the list do this:
-	for each in first_names:
-
+	for first, last in zip( first_names, last_names ):
 		#Create the users
-		user = User.objects.create_user(, 'lennon@thebeatles.com', 'johnpassword')
-		user.last_name = 'Lennon'
-		user.email = self.cleaned_data['email']
-		user.first_name = self.cleaned_data['first_name']
-		user.last_name =
-		user.institution =
+		user = User.objects.create_user( first, 'email@email.com', 'password')
+		user.last_name = last
+		user.institution = "FSU"
 		user.save()
-
 		new_student = Student( user=new_user, fName=new_user.first_name, lName=new_user.last_name, Institution=new_user.institution )  
 		new_student.save()
 
 		#Add some classes
 		#loop through and create class 1, 2, 3, 4
-		new_student.course_set.create( course_name = course_to_save.course_name , course_code =course_to_save.course_code  )
-
-		#Add activities to each class
-		for course in new_student.course_set.all():
+		for course in sampleClasses:
+			new_course = new_student.course_set.create( course_name = course[0] , course_code =course[1]  )
+			#Add activities to each class
 			#add activity 1, 2, 3, with random grades, weights, and all due Random Days in May
 			for activity in sample_activities:
-				new_activity = course.graded_activities_set.create( activity_name = form.cleaned_data['activityName'], grade_weight = form.cleaned_data['activityWeight'], grade_earned=form.cleaned_data['activity_Grade_Earned'] )
+				new_activity = new_course.graded_activities_set.create( 	activity_name = activity , 
+												grade_weight = random.random(), 
+												grade_earned=random.random(),
+												grade_due_date = date.today + timedelta( days=random.randint(1,50) )  )
 				#for each new activity add some subactivities
 				for subactivity in sample_subactivities:
-					new_activity.subgraded_activities_set.create(activity_name = form.cleaned_data['activityName'], grade_weight = form.cleaned_data['activityWeight'], grade_earned=form.cleaned_data['activity_Grade_Earned']  )
-	"""
+					new_activity.subgraded_activities_set.create( 	activity_name = subactivity , 
+											subgrade_weight = random.random(), 
+											subgrade_earned=random.random(),
+											subgrade_due_date = date.today + timedelta( days=random.randint(1,50) )  )
+	
 
 
